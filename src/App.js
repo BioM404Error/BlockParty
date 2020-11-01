13bedfb8b624a673c0b7181df1ae8f211f48e505
Bridget <bridgetoconnor808@gmail.com>
2020-10-31 20:04:48 -1000

    Removing the milestones from master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Milestone1Embedded from "./milestones/milestone1embedded";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Milestone1Embedded />
+      <h1>Hello World</h1>
     </div>
   );
 }
