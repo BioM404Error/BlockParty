90638312a9415fcd386673f8553c96d9c37d4d2d
Isaiah <anyiminene@gmail.com>
2020-11-07 21:28:48 -0800

    added r3f and drei

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Canvas } from "react-three-fiber";
+import styled from "@emotion/styled";
+
+const CanvasSpace = styled.div`
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  display: block;
+`
 
 const Landing = () => {
   return (
-    <></>
+    <CanvasSpace>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <mesh>
+          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" color="red" />
+        </mesh>
+      </Canvas>
+    </CanvasSpace>
   )
 }
 
