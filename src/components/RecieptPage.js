030ab3036d1df6ccab73eccf92b93d60a1179587
Onariaginosa <ona.igbinedion@gmail.com>
2020-11-14 22:54:10 -0800

    completed form validation for send recipt modal

--- src/components/RecieptPage.js
+++ src/components/RecieptPage.js
@@ -6,22 +6,31 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Modal from "react-bootstrap/Modal";
-import { Link } from "react-router-dom";
+import InputGroup from "react-bootstrap/InputGroup";
+import { useHistory } from "react-router-dom";
+
 
 export default function Reciept(props) {
   const context = useContext(Context);
   const serviceFee =
     context.cart.reduce((acc, cur) => acc + cur.price, 0) * 0.05;
   const [show, setShow] = useState(false);
-  const [email, setEmail] = useState(undefined);
-  const [phone, setPhone] = useState(undefined);
+  const history = useHistory();
+  const [validated, setValidated] = useState(false);
+  
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    setValidated(true);
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      history.push("/thankyou");
+    }
 
-  //TODO: Throw an error if the user doesnt imput a phone number or an email
+  };
 
   const handleClose = () => {
-    setShow(false);
-  };
-  const handleSend = () => {
     setShow(false);
   };
   const handleShow = () => setShow(true);
@@ -74,33 +83,40 @@ export default function Reciept(props) {
           <Modal.Title>Enter Email</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group controlId="formEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+              <InputGroup>
+              <Form.Control
+                required
+                type="email"
+                placeholder="jane.doe@organization.com" />
+              <Form.Control.Feedback type="invalid">
+                  Please choose a valid email.
+              </Form.Control.Feedback>
+              </InputGroup>
             </Form.Group>
             <Form.Group controlId="formPhoneNumber">
               <Form.Label>Phone Number</Form.Label>
               <Form.Control
+                required
                 type="tel"
                 pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                 placeholder="Enter Phone Number ###-###-###"
               />
-              <Form.Text className="text-muted">
-                We'll never share your number with anyone else.
-              </Form.Text>
+              <Form.Control.Feedback type="invalid">
+               Please insert a valid phone number.
+              </Form.Control.Feedback>
             </Form.Group>
+            <Form.Row>
+            <Button variant="primary" type="submit" >
+              Send
+          </Button>
+            </Form.Row>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" as={Link} to="/thankyou">
-            Send
-          </Button>
-        </Modal.Footer>
       </Modal>
     </div>
   );
 }
+
