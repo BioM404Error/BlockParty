502afc9a10665d22a3adf4e911de420009729c95
Onariaginosa <ona.igbinedion@gmail.com>
2020-11-24 11:03:36 -0800

    fixed small business page authentication and added back back button

--- src/components/SmallBusinessPage.js
+++ src/components/SmallBusinessPage.js
@@ -6,7 +6,7 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export default function SmallBusinessPage(props) {
   const [show, setShow] = useState(false);
@@ -14,7 +14,21 @@ export default function SmallBusinessPage(props) {
     setShow(false);
   };
 
+  const history = useHistory();
+  const [validated, setValidated] = useState(false);
+
   const handleShow = () => setShow(true);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    setValidated(true);
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      history.push("/smallbusiness/thankyou");
+    }
+  }
 
   return (
     <div>
@@ -22,7 +36,7 @@ export default function SmallBusinessPage(props) {
         <Card.Body>
           <h1>Benefits of Joining Us</h1>
           <p>
-           Small Business's are the heart of every community. They go the extra
+            Small Business's are the heart of every community. They go the extra
             mile, and have always been there for their communities. On The Block
             wants to return the favor, and be there for them. When you join us,
             your small business will be filtered by what type of business you
@@ -34,6 +48,9 @@ export default function SmallBusinessPage(props) {
             be seen by many different people and spread your business magic to a
             larger audience.
           </p>
+          <Button variant="secondary" as={Link} to="/">
+            Home
+          </Button>
           <Button variant="primary" onClick={handleShow}>
             Join Us!
           </Button>
@@ -45,13 +62,16 @@ export default function SmallBusinessPage(props) {
           <Modal.Title>We Are Happy To Connect </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group as={Row}>
               <Form.Label style={{ display: "flex" }} column sm="2">
                 Name <p style={{ color: "red" }}>*</p>
               </Form.Label>
               <Col sm="10">
-                <Form.Control required placeholder="Enter Name" />
+                <Form.Control required type="text" placeholder="Enter Name" />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid name. Ex: Jane Doe
+                </Form.Control.Feedback>
               </Col>
             </Form.Group>
             <Form.Group as={Row}>
@@ -61,6 +81,9 @@ export default function SmallBusinessPage(props) {
               </Form.Label>
               <Col sm="10">
                 <Form.Control required type="email" placeholder="Enter Email" />
+                <Form.Control.Feedback type="invalid">
+                  Please choose a valid email. Ex: jane.doe@organization.com
+                </Form.Control.Feedback>
               </Col>
             </Form.Group>
             <Form.Group as={Row}>
@@ -73,6 +96,9 @@ export default function SmallBusinessPage(props) {
                   pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                   placeholder="Enter Phone Number ###-###-###"
                 />
+                <Form.Control.Feedback type="invalid">
+                  Please insert a valid phone number. Ex: 012-345-6789
+                </Form.Control.Feedback>
               </Col>
             </Form.Group>
             <Form.Group as={Row}>
@@ -81,6 +107,9 @@ export default function SmallBusinessPage(props) {
               </Form.Label>
               <Col sm="10">
                 <Form.Control type="url" placeholder="Enter link" />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid social media url. Ex: https://www.instagram.com/JanesDoenuts
+                </Form.Control.Feedback>
               </Col>
             </Form.Group>
             <Form.Group>
@@ -89,7 +118,7 @@ export default function SmallBusinessPage(props) {
               </Form.Label>
               <Form.Control as="textarea" rows={3}></Form.Control>
             </Form.Group>
-            <Button variant="primary" as={Link} to="/smallbusiness/thankyou">
+            <Button variant="primary" type="submit">
               Send
             </Button>
           </Form>
